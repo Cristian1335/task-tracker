79a6b1cdc0c1e9209a7de96ddfefd792c38a67f8
Cristian1335 <cristifigue@hotmail.com>
2026-06-06 18:13:59 -0300

    Persistencia de datos con JSON, generacion de IDs unicos y manejo de errores. Incorporar funcionalidades de consola

--- main.py
+++ main.py
@@ -1,48 +1,107 @@
-#Task tracker aplicando el principio de responsabilidad única (SRP) y POO
+#Task tracker aplicando persistencia de datos en un archivo JSON. El programa debe permitir al usuario agregar, actualizar, eliminar y listar tareas, así como marcar tareas como "en progreso" o "hecho". Cada tarea debe tener un nombre, una descripción, un estado (pendiente, en progreso o hecho) y una fecha de creación y actualización. Además utilizo mmanejo de errores, principios de POO para estructurar mejor las tareas y aplico patrones de diseño para mejorar las clases aplicadas y evitar acoplamiento y redundancia.
+# #La generacion de id se hace mediante la deteccion del ultimo id registrado en la lista de tareas, al cual se le incrementa 1 unidad para generar el nuevo id, Esto garantiza que no se repitan id y que el sistema de identificación de tareas sea robusto y confiable, incluso después de eliminar tareas. 
+
+#Requirements
+#The application should run from the command line, accept user actions and inputs as arguments, and store the tasks in a JSON file. The user should be able to:
+#Add, Update, and Delete tasks
+#Mark a task as in progress or done
+#List all tasks   --hecho
+#List all tasks that are done     
+#List all tasks that are not done 
+#List all tasks that are in progress 
+
+import json 
+import datetime
 
 class Task:
-    def __init__(self, name, description, status, createdAt, updatedAt):
+    def __init__(self, id,name, description, status="to do", createdAt=None, updatedAt=None):
+        self.id = id
         self.name = name
         self.description = description
         self.status = status
-        self.createdAt = createdAt
-        self.updatedAt = updatedAt
+        self.createdAt = createdAt if createdAt else datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.updatedAt = updatedAt if updatedAt else datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
     def __str__(self):
-        return f"Nombre: {self.name} Descripcion: {self.description}\nEstado: {self.status}\nCreada el: {self.createdAt} \nActualizada el: {self.updatedAt}\n"
+        return f"ID: {self.id}\nNombre: {self.name} -- Descripcion: {self.description}\nEstado: {self.status}\nCreada el: {self.createdAt} \nActualizada el: {self.updatedAt}\n"
+    
+    def convertir_a_diccionario(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "status": self.status,
+            "createdAt": self.createdAt,
+            "updatedAt": self.updatedAt
+        }
 
 class TaskManager:
     def __init__(self):
-        self.tasks = []
-
-    def AddTask(self, name, description, status, createdAt, updatedAt):
-        newTask = Task(name, description, status, createdAt, updatedAt)
+        self.tasks = [] #lista de tareas
+        self.LoadTasks() #cargo las tareas al iniciar el programa
+    
+    def AddTask(self, name, description, status="to do", createdAt=None, updatedAt=None):
+        if len(self.tasks) == 0:
+            id = 1
+        else:
+            id = self.tasks[-1].id + 1 #sumo +1 al ultimo id de la lista
+        newTask = Task(id, name, description, status, createdAt, updatedAt)
         self.tasks.append(newTask)
+        self.SaveTasks() #guardo las tareas cada vez que agrego una nueva
         print(f"Tarea {name} agregada")
 
     def GetTasks(self):
         for task in self.tasks:
             print(task)
 
-    def UpdateTask(self, name, description, status, updatedAt): 
+    def LoadTasks(self):
+        try:
+            with open("tasks.json", "r") as archivo:
+                tareas_guardadas = json.load(archivo)
+                for item in tareas_guardadas:
+                    tarea = Task(id=item["id"], name=item["name"], description=item["description"], status=item["status"], createdAt=item["createdAt"], updatedAt=item["updatedAt"])
+                    self.tasks.append(tarea)
+        except FileNotFoundError:
+            pass #si no existe el archivo, no hace nada ya que la lista de tareas está vacía
+
+    def UpdateTask(self, id, description, status): 
         for task in self.tasks:
-            if task.name == name:
+            if task.id == id:
                 task.description = description
                 task.status = status
-                task.updatedAt = updatedAt
-                print(f"Tarea {name} actualizada")
-            else:
-                print(f"No se encontró la tarea {name}")
+                task.updatedAt = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                self.SaveTasks() #guardo las tareas cada vez que actualizo una tarea
+                print(f"Tarea {task.name} actualizada")
+                return
+        #se ejecuta en caso de no encontrarla en el for
+        print(f"No se encontró la tarea con id {id}")
     
-    def DeleteTask(self, name):
+    def DeleteTask(self, id):
         for task in self.tasks:
-            if task.name == name:
+            if task.id == id:
                 self.tasks.remove(task)
-                print(f"Tarea {name} eliminada")
-            else: 
-                print(f"No se encontró la tarea {name}")
+                self.SaveTasks() #guardo las tareas cada vez que elimino una tarea
+                print(f"Tarea {task.name} eliminada")
+                return
+        print(f"No se encontró la tarea con id {id}")
+
+    def SaveTasks(self):
+        lista_a_guardar = [] #Creo una lista vacía para guardar las tareas convertidas a diccionario
+        for task in self.tasks:
+            tarea = task.convertir_a_diccionario() #recorro cada tarea y la convierto en diccionario
+            lista_a_guardar.append(tarea)
+        with open("tasks.json","w") as archivo:
+            json.dump(lista_a_guardar, archivo, indent=4) #guardo la lista en un json
+            print("Tareas guardadas correctamente")
+
+    def ListTasksByStatus(self, status):
+        for task in self.tasks:
+            if task.status == status:
+                print(task)
 
 manager = TaskManager()
-manager.AddTask("Tarea 1", "Descripción de la tarea 1", "Pendiente", "2024-06-01", "2024-06-01")
-manager.AddTask("Tarea 2", "Descripción de la tarea 2", "En progreso", "2024-06-02", "2024-06-02")
+#manager.AddTask("Tarea 1", "Descripción de la tarea 1", "Pendiente")
+#manager.AddTask("Tarea 2", "Descripción de la tarea 2", "En progreso", "2024-06-02", "2024-06-02")
 manager.GetTasks()
+#manager.DeleteTask(5)
 
